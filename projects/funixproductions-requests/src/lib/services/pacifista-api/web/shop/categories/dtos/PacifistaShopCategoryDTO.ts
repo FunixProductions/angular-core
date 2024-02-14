faaa14fa9d3d9f923e47bd9b8f9d504cc1b16dec
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-02-14 11:32:50 +0100

    add multi purchase

--- projects/funixproductions-requests/src/lib/services/pacifista-api/web/shop/categories/dtos/PacifistaShopCategoryDTO.ts
+++ projects/funixproductions-requests/src/lib/services/pacifista-api/web/shop/categories/dtos/PacifistaShopCategoryDTO.ts
@@ -3,4 +3,5 @@ import {ApiDTO} from "../../../../../core/dtos/api-dto";
 export class PacifistaShopCategoryDTO extends ApiDTO {
     name?: string;
     description?: string;
+    multiPurchaseAllowed?: boolean;
 }

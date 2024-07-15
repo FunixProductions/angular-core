30d8c49f709b7d750989c4adc8f1d215e68414fd
FunixG <antoine.pronnier@gmail.com>
2024-07-15 21:20:05 +0200

    added vote lib V1

---
+++ projects/funixproductions-requests/src/lib/services/pacifista-api/web/vote/dtos/VoteWebsiteDTO.ts
@@ -0,0 +1,6 @@
+export class VoteWebsiteDTO {
+    enumName: string;
+    displayName: string;
+    urlVote: string;
+    coolDownInMinutes: number;
+}
---
+++ projects/funixproductions-requests/src/lib/services/pacifista-api/web/vote/dtos/VotesCountDTO.ts
@@ -0,0 +1,4 @@
+export class VotesCountDTO {
+    username: string;
+    votesCount: number;
+}

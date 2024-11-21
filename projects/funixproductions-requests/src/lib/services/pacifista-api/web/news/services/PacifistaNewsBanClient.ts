4516923819901b1446d27ce480f466f5937a2bc4
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-11-21 01:38:23 +0100

    Done bans service

--- projects/funixproductions-requests/src/lib/services/pacifista-api/web/news/services/PacifistaNewsBanClient.ts
+++ projects/funixproductions-requests/src/lib/services/pacifista-api/web/news/services/PacifistaNewsBanClient.ts
@@ -1,3 +1,17 @@
-export class PacifistaNewsBanClient {
+import {CrudHttpClient} from "../../../../core/components/requests/crud-http-client";
+import {PacifistaNewsBanDTO} from "../dtos/ban/PacifistaNewsBanDTO";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../../../environments/environment";
+import {environmentDev} from "../../../../../../environments/environment-dev";
+
+export class PacifistaNewsBanClient extends CrudHttpClient<PacifistaNewsBanDTO> {
+
+    constructor(http: HttpClient, production: boolean) {
+        super(
+            http,
+            production ? environment.pacifistaApiUrl : environmentDev.pacifistaApiUrl,
+            'web/news/bans'
+        );
+    }
 
 }

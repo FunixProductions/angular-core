f87d2328701103b89e118a0f57847c8848c06609
iGameCreep <gamecreep35@gmail.com>
2025-02-27 22:21:43 +0100

    Fix relative imports

--- projects/funixproductions-requests/src/lib/services/pacifista-api/server/players/sync/service/PlayerMoneyService.ts
+++ projects/funixproductions-requests/src/lib/services/pacifista-api/server/players/sync/service/PlayerMoneyService.ts
@@ -1,8 +1,8 @@
 import {HttpClient} from "@angular/common/http";
-import {CrudHttpClient} from "projects/funixproductions-requests/src/public-api";
+import {CrudHttpClient} from "../../../../../core/components/requests/crud-http-client";
 import {PlayerMoneyDTO} from "../dtos/PlayerMoneyDTO";
-import {environment} from "projects/funixproductions-requests/src/environments/environment";
-import {environmentDev} from "projects/funixproductions-requests/src/environments/environment-dev";
+import {environment} from "../../../../../../../environments/environment";
+import {environmentDev} from "../../../../../../../environments/environment-dev";
 
 export class PlayerMoneyService extends CrudHttpClient<PlayerMoneyDTO> {
   constructor(protected httpClient: HttpClient, production: boolean) {

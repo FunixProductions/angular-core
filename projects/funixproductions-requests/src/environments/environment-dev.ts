4010bc773bce33201a04e59aecc4c972176765b6
Antoine PRONNIER <antoine.pronnier@gmail.com>
2025-06-09 14:33:16 +0200

    update env and update libs

--- projects/funixproductions-requests/src/environments/environment-dev.ts
+++ projects/funixproductions-requests/src/environments/environment-dev.ts
@@ -1,11 +1,11 @@
 export const environmentDev = {
   //Urls HTTP for dev.apis
-  funixgamingApiUrl: 'https://dev.api.funixgaming.fr/',
-  funixproductionsApiUrl: 'https://dev.api.funixproductions.com/',
-  pacifistaApiUrl: 'https://dev.api.pacifista.fr/',
+  funixgamingApiUrl: 'https://dev-api.funixgaming.fr/',
+  funixproductionsApiUrl: 'https://dev-api.funixproductions.com/',
+  pacifistaApiUrl: 'https://dev-api.pacifista.fr/',
 
   //Urls websocket for dev.apis
-  funixgamingWebsocketUrl: 'wss://dev.api.funixgaming.fr/',
-  funixproductionsWebsocketUrl: 'wss://dev.api.funixproductions.com/',
-  pacifistaWebsocketUrl: 'wss://dev.api.pacifista.fr/',
+  funixgamingWebsocketUrl: 'wss://dev-api.funixgaming.fr/',
+  funixproductionsWebsocketUrl: 'wss://dev-api.funixproductions.com/',
+  pacifistaWebsocketUrl: 'wss://dev-api.pacifista.fr/',
 }
